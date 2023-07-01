4aeeeb2105d1365cf68b5243cd4513791298fe28
Nataiia Vodopianova <natalvod@yandex.ru>
2023-07-01 19:00:01 +0300

    init test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+  }
+};
